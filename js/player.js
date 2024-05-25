2dc1ad80da76b2d1366f489284b749855f5ff05c
alexpodelinskii <nikitik00787@gmail.com>
2024-05-26 01:44:41 +0300

    feat: add playlists container? info and edit panel

--- js/player.js
+++ js/player.js
@@ -77,11 +77,17 @@ function Main() {
     playlistPanel.append(
         panelTitle,
         panelButton
-    )
+    );
+
+    const playlists = createElem('playlists');
+    data.playlists.forEach(elem => {
+        playlists.append(draftPlaylist(elem));
+    })
 
     main.append(
-        playlistPanel
-    )
+        playlistPanel,
+        playlists
+    );
 
 
 
@@ -89,10 +95,91 @@ function Main() {
 }
 
 
-function createElem(classname, element = 'div') {
-    const elem = document.createElement(element);
-    elem.classList.add(classname);
-    return elem;
+
+function draftPlaylist(playist) {
+    const playlist = createElem('playlist', 'article');
+
+    const trackList = createElem('tracklist');
+
+    trackList.append(
+        draftTracklistPanel(),
+    )
+
+    playlist.append(
+        draftPlaylistInfo(playist.playlistIfo),
+        trackList
+    )
+
+    return playlist;
+}
+
+
+function draftPlaylistInfo(info) {
+    const playlistInfo = createElem('playlist-info');
+
+    const playlistCoverImage = createImg('playlist-cover-image', info.coverImgUrl, 'cover');
+
+    const playlistDescription = createElem('playlist-description');
+
+    const playlistTitle = createElem('title', 'h2');
+    playlistTitle.innerText = info.title;
+
+    const playlistCounts = createElem('tracks-count');
+    playlistCounts.innerText = `${info.tracksCount} tracks`;
+
+    playlistDescription.append(
+        playlistTitle,
+        playlistCounts
+    )
+
+    playlistInfo.append(
+        playlistCoverImage,
+        playlistDescription,
+        draftEditPanel()
+    )
+    return playlistInfo;
+}
+
+function draftEditPanel() {
+    const panel = createElem('buttons-container');
+
+    const editBtn = createElem('btn', 'button');
+    editBtn.append(
+        createImg('button-icon', 'img/icons/edit.svg', 'edit')
+    )
+
+    const delBtn = createElem('btn', 'button');
+    delBtn.append(
+        createImg('button-icon', 'img/icons/basket.svg', 'delete')
+    );
+
+    panel.append(
+        editBtn,
+        delBtn
+    )
+
+    return panel;
+}
+
+
+function draftTracklistPanel() {
+    const panel = createElem('add-track-panel');
+
+    const title = createElem('tracklist-title', 'h3');
+    title.innerText = 'Tracks';
+
+    const button = createElem('button-add-track', 'button');
+
+    button.append(
+        createImg('add-image', 'img/icons/add.svg', 'add'),
+        'Add Track'
+    );
+
+    panel.append(
+        title,
+        button
+    )
+    return panel;
 }
 
 
@@ -101,3 +188,18 @@ const root = document.getElementById('root');
 root.append(
     App()
 )
+
+
+
+function createElem(classname, element = 'div') {
+    const elem = document.createElement(element);
+    elem.classList.add(classname);
+    return elem;
+}
+
+function createImg(classname, src, alt) {
+    const image = createElem(classname, 'img');
+    image.src = src;
+    image.alt = alt;
+    return image;
+}
