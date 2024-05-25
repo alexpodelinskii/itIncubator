de6e32ff6ca01ebbb2d71e3c49ee34dc6a5eb8c8
alexpodelinskii <nikitik00787@gmail.com>
2024-05-26 00:44:51 +0300

    feat: draft header and main from JS

--- js/player.js
+++ js/player.js
@@ -28,4 +28,76 @@ const data = {
             tracks: []
         }
     ]
+};
+
+
+function App() {
+    const container = createElem('App');
+
+    container.append(
+        Header(),
+        Main()
+    )
+
+    return container;
+};
+
+function Header() {
+    const header = createElem('header', 'header');
+    const container = createElem('header-container');
+
+    const headerLogo = createElem('logoImg', 'img');
+    headerLogo.src = 'img/logo/logo.svg';
+    headerLogo.alt = 'logo';
+
+    const headerName = createElem('logo-name');
+    headerName.innerText = 'InPlayer';
+
+    container.append(
+        headerLogo,
+        headerName
+    );
+
+    header.append(container);
+
+    return header;
 }
+
+function Main() {
+    const main = createElem('main', 'main');
+
+    const playlistPanel = createElem('add-playlist-panel');
+
+    const panelTitle = createElem('title', 'h1');
+    panelTitle.innerText = 'My playlists';
+
+    const panelButton = createElem('button', 'button');
+    panelButton.innerText = 'Add Playlist';
+
+    playlistPanel.append(
+        panelTitle,
+        panelButton
+    )
+
+    main.append(
+        playlistPanel
+    )
+
+
+
+    return main;
+}
+
+
+function createElem(classname, element = 'div') {
+    const elem = document.createElement(element);
+    elem.classList.add(classname);
+    return elem;
+}
+
+
+
+const root = document.getElementById('root');
+root.append(
+    App()
+)
