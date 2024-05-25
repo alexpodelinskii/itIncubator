ba4f0b01071045f2a69b1e3df7e62908f32b24b1
alexpodelinskii <nikitik00787@gmail.com>
2024-05-25 23:44:53 +0300

    feat: add playlist info object

---
+++ js/player.js
@@ -0,0 +1,22 @@
+const data = {
+    playlists: [
+        {
+            playlistIfo: {
+                id: '1-0',
+                title: 'Hip-Hop Hits',
+                coverImgUrl: 'img/cardImage/image1.jpeg',
+                tracksCount: 4
+            },
+            tracks: []
+        },
+        {
+            playlistIfo: {
+                id: '1-0',
+                title: 'Rap Hits 1990s',
+                coverImgUrl: 'img/cardImage/image2.jpeg',
+                tracksCount: 4
+            },
+            tracks: []
+        }
+    ]
+}
